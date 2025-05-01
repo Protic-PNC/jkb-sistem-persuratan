001faf3534edf65d6d05b1b0abf4feccc7273126
Evan_arlen194 <cloaaa00@gmail.com>
2025-05-01 23:10:09 +0700

    Create import.blade.php

---
+++ resources/views/dashboard/admin/users/import.blade.php
@@ -0,0 +1,29 @@
+@extends('dashboard.admin.layouts.main')
+
+@section('container')
+    <div class="container-fluid mb-4 pt-4 px-4">
+        <div class="row g-4">
+            <div class="col-sm-12 col-xl-6">
+                <div class="bg-light rounded h-100 p-4">
+                    <h6 class="mb-4">Upload CSV</h6>
+                    <form action="/dashboard/admin/user/import" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        <div class="mb-3">
+                            <label for="csv_file" class="form-label">Pilih File CSV</label>
+                            <input type="file" class="form-control @error('csv_file') is-invalid @enderror" 
+                                id="csv_file" name="csv_file">
+                            @error('csv_file')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+                        <a href="/dashboard/admin/user" class="btn btn-success"><i class="bi bi-arrow-left-square"></i>
+                            Kembali</a>
+                        <button type="submit" class="btn btn-primary">Upload CSV</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
