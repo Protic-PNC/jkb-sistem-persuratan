62bfbfa58cd795151611caa4c31303e96dac1a37
Evan_arlen194 <cloaaa00@gmail.com>
2025-05-02 23:39:42 +0700

    Update web.php for import route

--- routes/web.php
+++ routes/web.php
@@ -65,6 +65,8 @@ Route::get('/dashboard/admin/user', [AdminUserController::class, 'index'])->midd
 Route::get('/dashboard/admin/user/create', [AdminUserController::class, 'create'])->middleware('admin');
 Route::get('/dashboard/admin/user/import', [AdminUserController::class, 'showImportForm'])->middleware('admin');
 Route::post('/dashboard/admin/user/import', [AdminUserController::class, 'importCSV'])->middleware('admin');
+Route::get('/dashboard/admin/kelas/import', [AdminKelasController::class, 'showImportForm'])->middleware('admin');
+Route::post('/dashboard/admin/kelas/import', [AdminKelasController::class, 'importCSV'])->middleware('admin');
 Route::post('/dashboard/admin/user', [AdminUserController::class, 'store'])->middleware('admin');
 Route::get('/dashboard/admin/user/{user}/edit', [AdminUserController::class, 'edit'])->middleware('admin');
 Route::put('/dashboard/admin/user/{user}', [AdminUserController::class, 'update'])->middleware('admin');
