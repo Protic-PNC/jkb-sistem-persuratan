813b523da26edcb889bbb54a32a1718ef08e1334
Evan_arlen194 <cloaaa00@gmail.com>
2025-05-03 22:41:24 +0700

    Update main.blade.php

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,7 +9,7 @@
     <meta content="" name="description">
 
     <!-- Favicon -->
-    <link href="{{ asset('img/iconWeb.png') }}" rel="icon">
+    <link href="{{ asset('img/jkb_logo.png') }}" rel="icon">
 
     <!-- Google Web Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
