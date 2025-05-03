f3470bbaa8362791034febde356d51fc00e685f6
Evan_arlen194 <cloaaa00@gmail.com>
2025-05-03 22:40:04 +0700

    Update import.blade.php

--- resources/views/dashboard/admin/kelas/import.blade.php
+++ resources/views/dashboard/admin/kelas/import.blade.php
@@ -1,9 +1,9 @@
 @extends('dashboard.admin.layouts.main')
 
 @section('container')
-    <div class="container-fluid mb-4 pt-4 px-4">
-        <div class="row g-4">
-            <div class="col-sm-12 col-xl-6">
+    <div class="container-fluid mb-4 pt-4 px-4 d-flex justify-content-center align-items-center">
+        <div class="row g-4 w-100">
+            <div class="col-sm-12 col-xl-6 mx-auto">
                 <div class="bg-light rounded h-100 p-4">
                     <h6 class="mb-4">Upload CSV</h6>
                     <form id="import-form-kelas" action="/dashboard/admin/kelas/import" method="POST"
@@ -21,8 +21,8 @@
                         </div>
                         <a href="/dashboard/admin/kelas" class="btn btn-success"><i class="bi bi-arrow-left-square"></i>
                             Kembali</a>
-                        <button type="submit" class="btn btn-primary"
-                            onclick= "event.preventDefault(); importKelasCSV()"><i class="bi bi-check2 me-1"></i>Import
+                        <button type="submit" class="btn btn-primary" onclick="event.preventDefault(); importKelasCSV()"><i
+                                class="bi bi-check2 me-1"></i>Import
                             CSV</button>
                     </form>
                 </div>
