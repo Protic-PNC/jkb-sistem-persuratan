140cd7b97f3c04d54587b0a16829f510464d77c6
Evan_arlen194 <cloaaa00@gmail.com>
2025-05-04 19:44:12 +0700

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -2180,3 +2180,18 @@ async function uploadMahasiswaSuratMagang() {
             });
         });
 }
+
+async function togglePassword(fieldId, btn) {
+    const input = document.getElementById(fieldId);
+    const icon = btn.querySelector("i");
+
+    if (input.type === "password") {
+        input.type = "text";
+        icon.classList.remove("bi-eye");
+        icon.classList.add("bi-eye-slash");
+    } else {
+        input.type = "password";
+        icon.classList.remove("bi-eye-slash");
+        icon.classList.add("bi-eye");
+    }
+}
