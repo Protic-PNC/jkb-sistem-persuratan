38100652e783bf427f93bb30cb4e45db91825629
Evan_arlen194 <cloaaa00@gmail.com>
2025-05-08 14:49:03 +0700

    Update PernyataanMagangController.php

--- app/Http/Controllers/Admin/PernyataanMagangController.php
+++ app/Http/Controllers/Admin/PernyataanMagangController.php
@@ -15,26 +15,26 @@ use Illuminate\Validation\ValidationException;
 class PernyataanMagangController extends Controller
 {
     public function index(Request $request)
-{
-    $pernyataans = PernyataanMagang::all();
-    $totalPernyataanMagang = PernyataanMagang::count();
-    $totalDiproses = PernyataanMagang::whereNull('status')->orWhere('status', 'diproses')->count();
-    $totalDisetujui = PernyataanMagang::where('status', 'approved')->count();
-    $totalDitolak = PernyataanMagang::where('status', 'rejected')->count();
+    {
+        $pernyataans = PernyataanMagang::all();
+        $totalPernyataanMagang = PernyataanMagang::count();
+        $totalDiproses = PernyataanMagang::whereNull('status')->orWhere('status', 'diproses')->count();
+        $totalDisetujui = PernyataanMagang::where('status', 'approved')->count();
+        $totalDitolak = PernyataanMagang::where('status', 'rejected')->count();
 
-    if ($request->ajax()) {
-        return view('dashboard.admin.pernyataan_magangs.table', compact('pernyataans'))->render();
+        if ($request->ajax()) {
+            return view('dashboard.admin.pernyataan_magangs.table', compact('pernyataans'))->render();
+        }
+
+        return view('dashboard.admin.pernyataan_magangs.index', [
+            'title' => 'Pernyataan Magang',
+            'pernyataans' => $pernyataans,
+            'totalPernyataanMagang' => $totalPernyataanMagang,
+            'totalDiproses' => $totalDiproses,
+            'totalDisetujui' => $totalDisetujui,
+            'totalDitolak' => $totalDitolak,
+        ]);
     }
-
-    return view('dashboard.admin.pernyataan_magangs.index', [
-        'title' => 'Pernyataan Magang',
-        'pernyataans' => $pernyataans,
-        'totalPernyataanMagang' => $totalPernyataanMagang,
-        'totalDiproses' => $totalDiproses,
-        'totalDisetujui' => $totalDisetujui,
-        'totalDitolak' => $totalDitolak,
-    ]);
-}
 
     public function create()
     {
@@ -43,44 +43,47 @@ class PernyataanMagangController extends Controller
         ]);
     }
 
-public function store(Request $request)
-{
-    try {
-        $validatedData = $request->validate([
-            'nama_ortu' => 'required|string|max:255',
-            'alamat' => 'required|string',
-            'no_telp' => 'required|string|max:255',
-            'nama_mhs' => 'required|string|max:255',
-            'username' => [
-                'required',
-                'string',
-                'max:255',
-                'unique:pernyataan_magangs,username',
-                function ($attribute, $value, $fail) {
-                    $user = User::where('username', $value)->first();
-                    if (!$user) {
-                        $fail("Username {$value} tidak ditemukan.");
-                    } elseif ($user->role_id !== 2) {
-                        $fail("Username {$value} bukan mahasiswa.");
-                    }
-                }
-            ],
-            'jurusan' => 'required|string|max:255',
-            'perguruan_tinggi' => 'required|string|max:255',
-            'tglSurat' => 'required|date',],
-            [
-                'username.unique' => 'Username tersebut sudah digunakan dalam surat pernyataan magang lain.',
-            ]);
+    public function store(Request $request)
+    {
+        try {
+            $validatedData = $request->validate(
+                [
+                    'nama_ortu' => 'required|string|max:255',
+                    'alamat' => 'required|string',
+                    'no_telp' => 'required|string|max:255',
+                    'nama_mhs' => 'required|string|max:255',
+                    'username' => [
+                        'required',
+                        'string',
+                        'max:255',
+                        'unique:pernyataan_magangs,username',
+                        function ($attribute, $value, $fail) {
+                            $user = User::where('username', $value)->first();
+                            if (!$user) {
+                                $fail("Username {$value} tidak ditemukan.");
+                            } elseif ($user->role_id !== 2) {
+                                $fail("Username {$value} bukan mahasiswa.");
+                            }
+                        }
+                    ],
+                    'jurusan' => 'required|string|max:255',
+                    'perguruan_tinggi' => 'required|string|max:255',
+                    'tglSurat' => 'required|date',
+                ],
+                [
+                    'username.unique' => 'Username tersebut sudah digunakan dalam surat pernyataan magang lain.',
+                ]
+            );
 
-        PernyataanMagang::create($validatedData);
+            PernyataanMagang::create($validatedData);
 
-        return response()->json(['message' => 'Data berhasil disimpan']);
-    } catch (ValidationException $e) {
-        return response()->json([
-            'message' => collect($e->errors())->flatten()->first()
-        ], 422);
+            return response()->json(['message' => 'Data berhasil disimpan']);
+        } catch (ValidationException $e) {
+            return response()->json([
+                'message' => collect($e->errors())->flatten()->first()
+            ], 422);
+        }
     }
-}
 
     public function checkUsername(Request $request)
     {
@@ -106,41 +109,46 @@ public function store(Request $request)
         ]);
     }
 
-public function update(Request $request, PernyataanMagang $pernyataanMagang)
-{
-    try {
-        $validatedData = $request->validate([
-            'nama_ortu' => 'required|string|max:255',
-            'alamat' => 'required|string',
-            'no_telp' => 'required|string|max:255',
-            'nama_mhs' => 'required|string|max:255',
-            'username' => [
-                'required', 'string', 'max:255',
-                function ($attribute, $value, $fail) {
-                    $user = User::where('username', $value)->first();
-                    if (!$user) {
-                        $fail("Username {$value} tidak ditemukan.");
-                    } elseif ($user->role_id !== 2) {
-                        $fail("Username {$value} bukan mahasiswa.");
-                    }
-                }
-            ],
-            'jurusan' => 'required|string|max:255',
-            'perguruan_tinggi' => 'required|string|max:255',
-            'tglSurat' => 'required|date',],
-            [
-                'username.unique' => 'Username tersebut sudah digunakan dalam surat pernyataan magang lain.',
-            ]);
+    public function update(Request $request, PernyataanMagang $pernyataanMagang)
+    {
+        try {
+            $validatedData = $request->validate(
+                [
+                    'nama_ortu' => 'required|string|max:255',
+                    'alamat' => 'required|string',
+                    'no_telp' => 'required|string|max:255',
+                    'nama_mhs' => 'required|string|max:255',
+                    'username' => [
+                        'required',
+                        'string',
+                        'max:255',
+                        function ($attribute, $value, $fail) {
+                            $user = User::where('username', $value)->first();
+                            if (!$user) {
+                                $fail("Username {$value} tidak ditemukan.");
+                            } elseif ($user->role_id !== 2) {
+                                $fail("Username {$value} bukan mahasiswa.");
+                            }
+                        }
+                    ],
+                    'jurusan' => 'required|string|max:255',
+                    'perguruan_tinggi' => 'required|string|max:255',
+                    'tglSurat' => 'required|date',
+                ],
+                [
+                    'username.unique' => 'Username tersebut sudah digunakan dalam surat pernyataan magang lain.',
+                ]
+            );
 
-        $pernyataanMagang->update($validatedData);
+            $pernyataanMagang->update($validatedData);
 
-        return response()->json(['message' => 'Data berhasil diubah']);
-    } catch (ValidationException $e) {
-        return response()->json([
-            'message' => collect($e->errors())->flatten()->first()
-        ], 422);
+            return response()->json(['message' => 'Data berhasil diubah']);
+        } catch (ValidationException $e) {
+            return response()->json([
+                'message' => collect($e->errors())->flatten()->first()
+            ], 422);
+        }
     }
-}
 
 
     public function destroy(PernyataanMagang $pernyataanMagang)
@@ -157,86 +165,86 @@ public function update(Request $request, PernyataanMagang $pernyataanMagang)
             'pernyataans' => $pernyataanMagang,
         ])->setPaper('a4', 'portrait');
 
-        return $pdf->stream('Surat Pernyataan Magang_' . $pernyataanMagang->nama_mhs .'_'. $pernyataanMagang->username .'_'. $pernyataanMagang->jurusan . '.pdf');
+        return $pdf->stream('Surat Pernyataan Magang_' . $pernyataanMagang->nama_mhs . '_' . $pernyataanMagang->username . '_' . $pernyataanMagang->jurusan . '.pdf');
     }
 
     public function uploadForm($id)
-{
-    $pernyataans = PernyataanMagang::findOrFail($id);
-    return view('dashboard.admin.pernyataan_magangs.upload', [
-    'title' => 'Pernyataan Magang',
-    'pernyataans' => $pernyataans
-]);
-}
-
-public function upload(Request $request, $id)
-{
-    if (!$request->hasFile('file_pdf')) {
-        return response()->json([
-            'status' => 'error',
-            'message' => 'Tidak ada file yang diupload.'
+    {
+        $pernyataans = PernyataanMagang::findOrFail($id);
+        return view('dashboard.admin.pernyataan_magangs.upload', [
+            'title' => 'Pernyataan Magang',
+            'pernyataans' => $pernyataans
         ]);
     }
 
-    $request->validate([
-        'file_pdf' => 'required|mimes:pdf|max:2048',
-    ]);
-
-    $pernyataans = PernyataanMagang::findOrFail($id);
-    $file = $request->file('file_pdf');
-
-    if ($pernyataans->file_pdf && Storage::disk('public')->exists($pernyataans->file_pdf)) {
-        $old = Storage::get('public/' . $pernyataans->file_pdf);
-        if (md5_file($file->getRealPath()) === md5($old)) {
+    public function upload(Request $request, $id)
+    {
+        if (!$request->hasFile('file_pdf')) {
             return response()->json([
-                'status' => 'info',
-                'message' => 'Tidak ada perubahan data yang dilakukan.'
+                'status' => 'error',
+                'message' => 'Tidak ada file yang diupload.'
             ]);
         }
+
+        $request->validate([
+            'file_pdf' => 'required|mimes:pdf|max:2048',
+        ]);
+
+        $pernyataans = PernyataanMagang::findOrFail($id);
+        $file = $request->file('file_pdf');
+
+        if ($pernyataans->file_pdf && Storage::disk('public')->exists($pernyataans->file_pdf)) {
+            $old = Storage::get('public/' . $pernyataans->file_pdf);
+            if (md5_file($file->getRealPath()) === md5($old)) {
+                return response()->json([
+                    'status' => 'info',
+                    'message' => 'Tidak ada perubahan data yang dilakukan.'
+                ]);
+            }
+        }
+
+        $path = $file->store('surat-magang', 'public');
+        $pernyataans->file_pdf = $path;
+        if ($pernyataans->status === 'rejected') {
+            $pernyataans->status = 'diproses';
+            $pernyataans->alasan = null;
+        }
+
+        $pernyataans->save();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'File berhasil diupload.'
+        ]);
     }
 
-    $path = $file->store('surat-magang', 'public');
-    $pernyataans->file_pdf = $path;
-    if ($pernyataans->status === 'rejected') {
-        $pernyataans->status = 'diproses';
+
+
+
+    public function setujui($id)
+    {
+        $pernyataans = PernyataanMagang::findOrFail($id);
+        $pernyataans->status = 'approved';
         $pernyataans->alasan = null;
+        $pernyataans->save();
+        $user = User::where('username', $pernyataans->username)->first();
+
+        Mail::to($user->email)->send(new StatusSuratPernyataanMagangChangedMail($pernyataans, 'approved'));
+
+        return response()->json(['message' => 'Surat disetujui.']);
     }
 
-    $pernyataans->save();
+    public function tolak(Request $request, $id)
+    {
+        $request->validate(['alasan' => 'required|string']);
+        $pernyataans = PernyataanMagang::findOrFail($id);
+        $pernyataans->status = 'rejected';
+        $pernyataans->alasan = $request->alasan;
+        $pernyataans->save();
+        $user = User::where('username', $pernyataans->username)->first();
 
-    return response()->json([
-        'status' => 'success',
-        'message' => 'File berhasil diupload.'
-    ]);
-}
+        Mail::to($user->email)->send(new StatusSuratPernyataanMagangChangedMail($pernyataans, 'rejected'));
 
-
-
-
-public function setujui($id)
-{
-    $pernyataans = PernyataanMagang::findOrFail($id);
-    $pernyataans->status = 'approved';
-    $pernyataans->alasan = null;
-    $pernyataans->save();
-    $user = User::where('username', $pernyataans->username)->first();
-
-    Mail::to($user->email)->send(new StatusSuratPernyataanMagangChangedMail($pernyataans, 'approved'));
-
-    return response()->json(['message' => 'Surat disetujui.']);
-}
-
-public function tolak(Request $request, $id)
-{
-    $request->validate(['alasan' => 'required|string']);
-    $pernyataans = PernyataanMagang::findOrFail($id);
-    $pernyataans->status = 'rejected';
-    $pernyataans->alasan = $request->alasan;
-    $pernyataans->save();
-    $user = User::where('username', $pernyataans->username)->first();
-
-    Mail::to($user->email)->send(new StatusSuratPernyataanMagangChangedMail($pernyataans, 'rejected'));
-
-    return response()->json(['message' => 'Surat ditolak.']);
-}
+        return response()->json(['message' => 'Surat ditolak.']);
+    }
 }
