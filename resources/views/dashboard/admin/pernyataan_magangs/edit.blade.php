6b0bb0aabd344759c3d45e88ff18d92aaa25cd06
Evan_arlen194 <cloaaa00@gmail.com>
2025-05-09 20:21:53 +0700

    Update edit.blade.php

--- resources/views/dashboard/admin/pernyataan_magangs/edit.blade.php
+++ resources/views/dashboard/admin/pernyataan_magangs/edit.blade.php
@@ -69,7 +69,8 @@
                         <div class="mb-3">
                             <label for="username" class="form-label">NPM</label>
                             <input type="text" class="form-control @error('username') is-invalid @enderror"
-                                id="username" name="username" value="{{ old('username', $pernyataans->username) }}">
+                                id="username" name="username" value="{{ old('username', $pernyataans->username) }}"
+                                oninput="autofillNamaMahasiswa()" onchange="autofillNamaMahasiswa()">
                             @error('username')
                                 <div class="invalid-feedback">
                                     {{ $message }}
@@ -117,3 +118,25 @@
         </div>
     </div>
 @endsection
+
+<script>
+function autofillNamaMahasiswa() {
+    var npmInput = document.getElementById('username');
+    var namaMhsInput = document.getElementById('nama_mhs');
+    if (npmInput && namaMhsInput) {
+        var npm = npmInput.value;
+        if (npm) {
+            fetch(`/dashboard/admin/user/get-mahasiswa-by-npm?npm=${npm}`)
+                .then(response => response.json())
+                .then(data => {
+                    namaMhsInput.value = data.nama_mhs || '';
+                })
+                .catch(() => {
+                    namaMhsInput.value = '';
+                });
+        } else {
+            namaMhsInput.value = '';
+        }
+    }
+}
+</script>
