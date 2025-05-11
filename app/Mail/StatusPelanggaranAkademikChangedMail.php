b191dfd0a9b610f7f5eba1e1cad08468e0280e32
Evan_arlen194 <cloaaa00@gmail.com>
2025-05-11 13:36:18 +0700

    Update StatusPelanggaranAkademikChangedMail.php

--- app/Mail/StatusPelanggaranAkademikChangedMail.php
+++ app/Mail/StatusPelanggaranAkademikChangedMail.php
@@ -13,31 +13,45 @@ class StatusPelanggaranAkademikChangedMail extends Mailable
 
     public $pelanggaran;
     public $status;
+    public $isMahasiswa;
+    public $opsi;
 
-    public function __construct(PelanggaranAkademik $pelanggaran, $status)
+    public function __construct(PelanggaranAkademik $pelanggaran, $status, $opsi = [])
     {
         $this->pelanggaran = $pelanggaran;
         $this->status = $status;
+        $this->opsi = $opsi;
     }
+
 
     public function build()
-{
-    $subject = '';
-    $view = '';
+    {
+        $subject = '';
+        $view = '';
 
-    if ($this->status == 'approved' || $this->status == 'rejected') {
-        $subject = 'Status Pelanggaran Akademik Anda';
-        $view = 'emails.status_pelanggaran';
-    } elseif ($this->status == 'reminder_ttd_pelanggaran') {
-        $subject = 'Pengingat Tanda Tangan Surat Pelanggaran Akademik';
-        $view = 'emails.reminder_ttd_pelanggaran';
+        if ($this->status == 'approved' || $this->status == 'rejected') {
+            $subject = 'Status Pelanggaran Akademik Anda';
+            $view = 'emails.status_pelanggaran';
+        } elseif ($this->status == 'reminder_ttd_pelanggaran') {
+            $subject = 'Pengingat Tanda Tangan Surat Pelanggaran Akademik';
+            $view = 'emails.reminder_ttd_pelanggaran';
+        } elseif ($this->status == 'reminder_approval') {
+            $subject = 'Pengingat Persetujuan Surat Pelanggaran Akademik';
+            $view = 'emails.reminder_approval_pelanggaran';
+        } elseif ($this->status == 'rejected_update') {
+            $subject = 'Tambahan Alasan Tanda Tangan Surat Pelanggaran Akademik';
+            $view = 'emails.alasan_pelanggaran';
+        } elseif ($this->status == 'data_changed') {
+            $subject = 'Perubahan Data Pelanggaran Akademik';
+            $view = 'emails.data_changed_pelanggaran';
+        }
+
+        return $this->subject($subject)
+            ->view($view)
+            ->with([
+                'pelanggaran' => $this->pelanggaran,
+                'isMahasiswa' => $this->isMahasiswa ?? false,
+                'opsi' => $this->opsi ?? [],
+            ]);
     }
-
-    return $this->subject($subject)
-                ->view($view)
-                ->with([
-                    'pelanggaran' => $this->pelanggaran,
-                ]);
-}
-
 }
